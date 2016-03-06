7b51326441bfd66cbe24c5f973ea800b919fc237
Mario Kaack <mario@shortrope.com>
2016-03-06 08:34:23 -0800

    Comment mkex.sh script

--- mkex.sh
+++ mkex.sh
@@ -2,9 +2,13 @@
 
 # mkex.sh   // make executible
 
+# file name specified?
 if [ $1 ]; then
+    # Make the specified file executable
     chmod +x $1
 else
+    # Make executable, files that have been modified w/in the last 3 minutes
+    # and the name has a .sh extension, in the current directory
     chmod +x `find ./ -cmin -3 -name "*.sh"`
 fi
 
